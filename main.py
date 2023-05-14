33e21f76fe79a29aa5bdf4f6eac2e14a57bcdcd5
ProrokWielki <ProrokWielki@o2.pl>
2023-05-14 18:48:54 +0200

    Added support for TkInter.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 import asyncio
 import signal
+from PIL import Image, UnidentifiedImageError
 
 from sliding_puzzle.drawing_engines.abstract_drawing_engine import Key
 from sliding_puzzle.drawing_engines.curses_drawing_engine import CursesDrawingEngine
+from sliding_puzzle.drawing_engines.tk_drawing_engine import TkDrawingEngine
 from sliding_puzzle.game_engine import GameEngine
-from sliding_puzzle.game_data_builders import CursesGameDataBuilder
+from sliding_puzzle.game_data_builders import TkGameDataBuilder, CursesGameDataBuilder
 
 
 def valid_size(size: int) -> int:
@@ -26,6 +28,25 @@ def valid_size(size: int) -> int:
     return size
 
 
+def valid_file(file: str) -> str:
+    """Validator for the image file argument of the SlidingPuzzle game.
+
+    Args:
+        file (str): file path to be validated.
+
+    Raises:
+        argparse.ArgumentTypeError: raised exception when the file path or file is invalid.
+
+    Returns:
+        str: valid file path.
+    """
+    try:
+        Image.open(file)
+    except (FileNotFoundError, UnidentifiedImageError) as exc:
+        raise argparse.ArgumentTypeError(f"{file} is not a valid image file.") from exc
+    return file
+
+
 async def main():
     """main function of the SlidingPuzzle game."""
     parser = argparse.ArgumentParser(prog="sliding_puzzle", description="SlidingPuzzle game.")
@@ -33,6 +54,7 @@ async def main():
     group = parser.add_mutually_exclusive_group(required=True)
 
     group.add_argument("--curses", action="store_true", help="Use curses to play the game.")
+    group.add_argument("--tk", dest="image_path", type=valid_file, help="Use tkinter to play the game")
 
     parser.add_argument(
         "SIZE", metavar="SIZE", type=valid_size, help="number of columns and rows (game board size is SIZE x SIZE)"
@@ -43,6 +65,14 @@ async def main():
     if args.curses:
         drawing_engine = CursesDrawingEngine()
         game_data = CursesGameDataBuilder.get(range(1, args.SIZE * args.SIZE), args.SIZE)
+
+    elif args.image_path:
+        drawing_engine = TkDrawingEngine()
+
+        image = Image.open(args.image_path)
+        image.thumbnail((800, 600))
+
+        game_data = TkGameDataBuilder.get(image, args.SIZE)
 
     drawing_engine.add_to_canvas(game_data.blank_block)
     for i in game_data.blocks:
